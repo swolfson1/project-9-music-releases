1d23b9523c06e15b10f7d012175a1999d179ea6e
sarah wolfson <swolfson23@gmail.com>
2019-12-04 11:29:45 +0800

    small changes

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export const Artist = (props) => {
   return (
-    <div className="artist">
-      <a href={props.url} className="artistLink">{props.name}</a>
-    </div>
+    <a href={props.url} className="artistLink">{props.name}</a>
+
   )
 }
